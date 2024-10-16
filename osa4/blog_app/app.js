8ca14fc3fbcbf05c4a856074e33d9a0ca9e73e26
pietarni <117778910+pietarni@users.noreply.github.com>
2024-10-16 15:33:46 +0300

    Osa4

--- osa4/blog_app/app.js
+++ osa4/blog_app/app.js
@@ -1,17 +1,27 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const { info, error } = require('./utils/logger')
 const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
+const usersRouter = require('./controllers/users')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
